666f6aa9fbc49e941c4177b3a43d8deb2f275bf5
Devlin Lynch <devklynch@gmail.com>
2024-09-05 19:31:53 -0600

    tests: add tests for fired upon and render for Cell

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -26,4 +26,59 @@ RSpec.describe Cell do
         expect(@cell.ship).to eq(@cruiser)
         expect(@cell.empty?).to eq(false)
     end
+###Tests Devlin added
+    it 'can check if its fired upon' do
+        @cruiser = Ship.new("Cruiser", 3)
+        @cell.place_ship(@cruiser)
+
+        expect(@cell.fired_upon?).to eq(false)
+    end
+
+    it 'can be fired upon' do
+        @cruiser = Ship.new("Cruiser", 3)
+        @cell.place_ship(@cruiser)
+
+        @cell.fire_upon 
+        expect(@cell.ship.health).to eq(2)
+        expected(@cell.fired_upon?).to eq(true)
+    end
+
+    it 'can render if the cell has not been fired upon' do
+        expect(@cell.render).to eq(".")
+        
+        @cell.fire_upon
+        expect(@cell.render).to eq("M")
+    end
+
+    it 'can reveal a ship in the cell even if it has been fired upon' do
+        @cell_2 = Cell.new("C3")
+        @cruiser = Ship.new("Cruiser", 3)
+        @cell_2.place_ship(@cruiser)
+
+        expect(@cell_2.render).to eq(".")
+        expect(@cell_2.render(true)).to eq("S")
+    end
+
+    it 'can render if the cell has been fired upon contains a ship' do
+        @cell_2 = Cell.new("C3")
+        @cruiser = Ship.new("Cruiser", 3)
+        @cell_2.place_ship(@cruiser)
+
+        @cell_2.fire_upon
+        expect(@cell_2.render).to eq("H")
+    end
+
+    it 'can render if the cell has been fired upon and its ship has been sunk'
+        @cell_2 = Cell.new("C3")
+        @cruiser = Ship.new("Cruiser", 3)
+        @cell_2.place_ship(@cruiser)
+        @cell_2.fire_upon
+
+        expect(@cruiser.sunk?).to eq(false)
+
+        @cruiser.hit
+        @cruiser.hit
+        expect(@cruiser.sunk?).to eq(true)
+        expect(@cell_2.render).to eq("X")
+
 end
