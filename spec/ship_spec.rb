13eba33449635ba6d1b20073d8a13196d946e9ac
Jonathan Atkins <jonathanjatkins89@gmail.com>
2024-09-05 17:09:00 -0700

    Initial Commit

---
+++ spec/ship_spec.rb
@@ -0,0 +1,13 @@
+require './lib/ship'
+
+RSpec.describe Ship do
+    before(:each) do
+        @cruiser = Ship.new("Cruiser", 3)
+    end 
+
+    it 'Ship exists and has attributes' do
+       expect(@cruiser).to be_an_instance_of(Ship)
+       expect(@cruiser.name).to eq("Cruiser")
+       expect(@cruiser.length).to eq(3)
+    end
+end
