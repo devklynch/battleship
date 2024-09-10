ef5ae00d34e8f9b8cff6caf4f527aadab24f16ba
Devlin Lynch <devklynch@gmail.com>
2024-09-09 19:19:48 -0600

    feat:updates from working session on game ship placement

--- lib/game.rb
+++ lib/game.rb
@@ -4,60 +4,95 @@ class Game
     attr_reader :computer_board,
                 :player_board,
                 :computer_cruiser,
-                :computer_submarine
+                :computer_submarine,
+                :player_cruiser,
+                :player_submarine
 
     def initialize
-        @computer_board = nil
-        @player_board = nil
-        @computer_cruiser = nil
-        @computer_submarine = nil
-    end
-
-    def new_game
-    p "Welcome to BATTLESHIP
-Enter p to play. Enter q to quit."
-    response = "p"
-        if response == "p"
         @computer_board = Board.new
         @player_board = Board.new
-        computer_submarine_placement
-        computer_cruiser_placement
-        "You are now playing the game"
-        elsif response == "q"
-            "You are not playing the game"
-        end
+        @computer_cruiser = Ship.new("Cruiser", 3)
+        @computer_submarine = Ship.new("Submarine", 2)
+        @player_cruiser = Ship.new("Cruiser", 3)
+        @player_submarine = Ship.new("Submarine", 2)
     end
 
+#     def new_game
+#     p "Welcome to BATTLESHIP
+# Enter p to play. Enter q to quit."
+#     response = gets.chomp
+#         if response == "p"
+#         computer_submarine_placement
+#         computer_cruiser_placement
+#         place_player_ship
+#         elsif response == "q"
+#             "You are not playing the game"
+#         end
+#     end
+
+    def new_game(response)
+        p "Welcome to BATTLESHIP
+            Enter p to play. Enter q to quit."
+            if response = "p"
+                computer_submarine_placement
+                computer_cruiser_placement
+                place_player_ship
+            elsif response == "q"
+                "You are not playing the game"
+            end
+    end
     def place_player_ship
-        p "I have laid out my ships on the grid.
-You now need to lay out your two ships.
-The Cruiser is three units long and the Submarine is two units long. #{@player_board.render}"
+        p "I have laid out my ships on the grid.You now need to lay out your two ships.\nThe Cruiser is three units long and the Submarine is two units long. #{@player_board.render}"
+
+        p "Please give coodinates for Cruiser(3 spaces)"
+        user_input = gets.chomp.downcase
+        until @player_board.valid_placement?(@player_cruiser,user_input)
+            p "Those are not valid coordinates"
+            user_input = gets.chomp.downcase
+        end
+        @player_board.place(@player_cruiser,user_input)
+        p "Player has placed Cruiser on #{user_input}"
+        
+        p "Please give coodinates for Submarine(2 spaces)"
+        user_input = gets.chomp.downcase
+        until @player_board.valid_placement?(@player_submarine,user_input)
+            p "Those are not valid coordinates"
+            user_input = gets.chomp.split
+        end
+        @player_board.place(submarine,user_input)
+        p "Player has placed Cruiser on #{user_input}"
     end
 
 
 
     def computer_submarine_placement
-        @computer_submarine = Ship.new("Submarine", 2)
-        orientation=["horizontal","vertical"].sample
-        if orientation == "horizontal"
-            column=[1,2,3].sample
-            row=["A","B","C","D"].sample
+#    def computer_placement(ship)
+        random_coordinates = @computer_board.cells.keys.sample(3)
 
-            ship_coordinates=[row+column.to_s, row +(column+1).to_s]
-            @computer_board.place(@computer_submarine,ship_coordinates)
+        until @computer_board.valid_placement?(ship,random_coordinates) 
+            random_coordinates = @computer_board.cells.keys.sample(3)
+        end
+        @computer_board.place(@computer_cruiser,random_coordinates)
+        # orientation=["horizontal","vertical"].sample
+        # if orientation == "horizontal"
+        #     column=[1,2,3].sample
+        #     row=["A","B","C","D"].sample
+
+        #     ship_coordinates=[row+column.to_s, row +(column+1).to_s]
+        #     @computer_board.place(@computer_submarine,ship_coordinates)
         
 
-        elsif orientation == "vertical"
-            column=["1","2","3","4"].sample
-            row=[65,66,67].sample
-            ship_coordinates= [row.chr+column, (row+1).chr+column]
-            #require "pry" ; binding.pry
-            @computer_board.place(@computer_submarine,ship_coordinates)
-        end
+        # elsif orientation == "vertical"
+        #     column=["1","2","3","4"].sample
+        #     row=[65,66,67].sample
+        #     ship_coordinates= [row.chr+column, (row+1).chr+column]
+        #     #require "pry" ; binding.pry
+        #     @computer_board.place(@computer_submarine,ship_coordinates)
+        # end
     end
 
     def computer_cruiser_placement
-        @computer_cruiser = Ship.new("Cruiser", 3)
+        #require "pry" ; binding.pry
         orientation=["horizontal","vertical"].sample
         if orientation == "horizontal"
             column=[1,2].sample
